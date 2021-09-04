6a948fb1342581e3b17b56bf254a7e27f16fcdf2
jjin <canglangke001@gmail.com>
2021-09-05 09:12:29 +1000

    DELIVERY-101 create project skeleton

---
+++ src/main/java/com/aummn/suburb/SuburbApplication.java
@@ -0,0 +1,13 @@
+package com.aummn.suburb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuburbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuburbApplication.class, args);
+	}
+
+}
